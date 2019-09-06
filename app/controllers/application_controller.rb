7773273fb1bed3f4f7064c076df7f9335987c661
河合宏紀 <hironori.kawai0720@gmail.com>
2019-09-06 14:05:54 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth, if: :production?
+  # before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
 
@@ -10,11 +10,11 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  #   end
+  # end
 
   # def configure_permitted_parameters
   #   devise_parameter_sanitizer.permit(:signup,keys:[])

3398dc7e0a6e0982ae9a018af04ed843ac95018b
河合宏紀 <hironori.kawai0720@gmail.com>
2019-09-07 18:03:10 +0900

    dbreset実行

--- db/migrate/20190906145207_add_user_id_to_cards.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToCards < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :cards, :user, foreign_key: true
-  end
-end
--- db/migrate/20190906145654_remove_user_from_cards.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserFromCards < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :cards, :user, :integer
-  end
-end

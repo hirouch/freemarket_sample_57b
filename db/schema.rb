4be41e12f86fb95e93b4edc2251c6998d5b5f2c0
河合宏紀 <hironori.kawai0720@gmail.com>
2019-09-08 09:13:35 +0900

    rake_db:migrate_resetによるスキーマファイルの変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190906145654) do
+ActiveRecord::Schema.define(version: 20190904073859) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -23,65 +23,67 @@ ActiveRecord::Schema.define(version: 20190906145654) do
     t.string   "address_name",                        null: false
     t.string   "address_block",                       null: false
     t.string   "address_building"
-    t.integer  "address_phone_number"
+    t.string   "address_phone_number"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["user_id"], name: "index_addresses_on_user_id", using: :btree
   end
 
   create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",     null: false
     t.string   "customer_id", null: false
     t.string   "card_id",     null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "user_id"
     t.index ["user_id"], name: "index_cards_on_user_id", using: :btree
   end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
+    t.string   "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "ancestry"
-    t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
   end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                        null: false
-    t.string   "image",                       null: false
-    t.text     "description",   limit: 65535, null: false
+    t.string   "name",                          null: false
+    t.string   "image",                         null: false
+    t.text     "description",     limit: 65535, null: false
     t.integer  "category_id"
-    t.string   "state",                       null: false
+    t.string   "state",                         null: false
     t.string   "size"
     t.string   "brand"
-    t.string   "postage",                     null: false
-    t.string   "region",                      null: false
-    t.string   "shipping_date",               null: false
-    t.integer  "price",                       null: false
+    t.string   "postage",                       null: false
+    t.string   "shipping_method"
+    t.string   "region",                        null: false
+    t.string   "shipping_date",                 null: false
+    t.integer  "price",                         null: false
     t.integer  "seller_id"
     t.integer  "buyer_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.index ["category_id"], name: "index_items_on_category_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "nickname",               null: false
-    t.string   "email",                  null: false
-    t.string   "last_name",              null: false
-    t.string   "first_name",             null: false
-    t.string   "last_name_kana",         null: false
-    t.string   "first_name_kana",        null: false
-    t.integer  "birthdate_year",         null: false
-    t.integer  "birthdate_month",        null: false
-    t.integer  "birthdate_day",          null: false
-    t.integer  "phone_number",           null: false
+    t.string   "nickname",                                          null: false
+    t.string   "email",                                             null: false
+    t.string   "image"
+    t.string   "last_name",                                         null: false
+    t.string   "first_name",                                        null: false
+    t.string   "last_name_kana",                                    null: false
+    t.string   "first_name_kana",                                   null: false
+    t.integer  "birthdate_year",                                    null: false
+    t.integer  "birthdate_month",                                   null: false
+    t.integer  "birthdate_day",                                     null: false
+    t.string   "phone_number",                                      null: false
+    t.text     "introduce",              limit: 65535
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.string   "encrypted_password"
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end

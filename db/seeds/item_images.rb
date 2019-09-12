dbb94085031a316887fdb48aed64e5ccf6e6fb01
axela5001 <axela0104@au.com>
2019-09-12 12:18:57 +0900

    seedファイル編集＆カラム名変更

---
+++ db/seeds/item_images.rb
@@ -0,0 +1,284 @@
+ItemImage.create(
+  [
+    {
+      item_id: '1',
+      image: 'https://static.mercdn.net/thumb/photos/m19860366014_1.jpg?1567742764',
+    },
+    {
+      item_id: '2',
+      image: 'https://static.mercdn.net/thumb/photos/m10943042487_1.jpg?1567742732',
+    },
+    {
+      item_id: '3',
+      image: 'https://static.mercdn.net/thumb/photos/m55407493672_1.jpg?1564473268',
+    },
+    {
+      item_id: '4',
+      image: 'https://static.mercdn.net/thumb/photos/m18858904589_1.jpg?1564462267',
+    },
+    {
+      item_id: '5',
+      image: 'https://static.mercdn.net/thumb/photos/m82847249683_1.jpg?1555728242',
+    },
+    {
+      item_id: '6',
+      image: 'https://static.mercdn.net/thumb/photos/m82700325530_1.jpg?1563458119',
+    },
+    {
+      item_id: '7',
+      image: 'https://static.mercdn.net/thumb/photos/m88743941876_1.jpg?1560238101',
+    },
+    {
+      item_id: '8',
+      image: 'https://static.mercdn.net/thumb/photos/m27559677202_1.jpg?1562500188',
+    },
+    {
+      item_id: '9',
+      image: 'https://static.mercdn.net/thumb/photos/m46334723137_1.jpg?1565673785',
+    },
+    {
+      item_id: '10',
+      image: 'https://static.mercdn.net/thumb/photos/m13830112259_1.jpg?1561347224',
+    },
+    {
+      item_id: '11',
+      image: 'https://static.mercdn.net/thumb/photos/m78840733107_1.jpg?1567740685',
+    },
+    {
+      item_id: '12',
+      image: 'https://static.mercdn.net/thumb/photos/m35033516877_1.jpg?1567740684',
+    },
+    {
+      item_id: '13',
+      image: 'https://static.mercdn.net/thumb/photos/m39449531257_1.jpg?1566973244',
+    },
+    {
+      item_id: '14',
+      image: 'https://static.mercdn.net/thumb/photos/m81062262660_1.jpg?1563693228',
+    },
+    {
+      item_id: '15',
+      image: 'https://static.mercdn.net/thumb/photos/m56618560896_1.jpg?1566809691',
+    },
+    {
+      item_id: '16',
+      image: 'https://static.mercdn.net/thumb/photos/m48481715106_1.jpg?1564152891',
+    },
+    {
+      item_id: '17',
+      image: 'https://static.mercdn.net/thumb/photos/m30252054609_1.jpg?1563808428',
+    },
+    {
+      item_id: '18',
+      image: 'https://static.mercdn.net/thumb/photos/m18903820178_1.jpg?1567404126',
+    },
+    {
+      item_id: '19',
+      image: 'https://static.mercdn.net/thumb/photos/m21572439951_1.jpg?1567298502',
+    },
+    {
+      item_id: '20',
+      image: 'https://static.mercdn.net/thumb/photos/m45712514152_1.jpg?1566699533',
+    },
+    {
+      item_id: '21',
+      image: 'https://static.mercdn.net/thumb/photos/m84941702622_1.jpg?1567685319',
+    },
+    {
+      item_id: '22',
+      image: 'https://static.mercdn.net/thumb/photos/m71477392886_1.jpg?1567519175',
+    },
+    {
+      item_id: '23',
+      image: 'https://static.mercdn.net/thumb/photos/m65524430191_1.jpg?1567746439',
+    },
+    {
+      item_id: '24',
+      image: 'https://static.mercdn.net/thumb/photos/m63102461817_1.jpg?1567746422',
+    },
+    {
+      item_id: '25',
+      image: 'https://static.mercdn.net/thumb/photos/m95117137859_1.jpg?1560655907',
+    },
+    {
+      item_id: '26',
+      image: 'https://static.mercdn.net/thumb/photos/m42828175118_1.jpg?1567356572',
+    },
+    {
+      item_id: '27',
+      image: 'https://static.mercdn.net/thumb/photos/m13714081942_1.jpg?1567746369',
+    },
+    {
+      item_id: '28',
+      image: 'https://static.mercdn.net/thumb/photos/m45439631004_1.jpg?1566974849',
+    },
+    {
+      item_id: '29',
+      image: 'https://static.mercdn.net/thumb/photos/m14905908510_1.jpg?1567746316',
+    },
+    {
+      item_id: '30',
+      image: 'https://static.mercdn.net/thumb/photos/m55913910014_1.jpg?1567687195',
+    },
+    {
+      item_id: '31',
+      image: 'https://static.mercdn.net/thumb/photos/m19918423643_1.jpg?1567739239',
+    },
+    {
+      item_id: '32',
+      image: 'https://static.mercdn.net/thumb/photos/m63123855714_1.jpg?1560863508',
+    },
+    {
+      item_id: '33',
+      image: 'https://static.mercdn.net/thumb/photos/m71229259100_1.jpg?1565955141',
+    },
+    {
+      item_id: '34',
+      image: 'https://static.mercdn.net/thumb/photos/m11717875247_1.jpg?1567739116',
+    },
+    {
+      item_id: '35',
+      image: 'https://static.mercdn.net/thumb/photos/m19738637297_1.jpg?1565509380',
+    },
+    {
+      item_id: '36',
+      image: 'https://static.mercdn.net/thumb/photos/m31314425956_1.jpg?1564578389',
+    },
+    {
+      item_id: '37',
+      image: 'https://static.mercdn.net/thumb/photos/m72260797691_1.jpg?1567261085',
+    },
+    {
+      item_id: '38',
+      image: 'https://static.mercdn.net/thumb/photos/m81163001985_1.jpg?1566723413',
+    },
+    {
+      item_id: '39',
+      image: 'https://static.mercdn.net/thumb/photos/m89925214351_1.jpg?1567493152',
+    },
+    {
+      item_id: '40',
+      image: 'https://static.mercdn.net/thumb/photos/m43999242273_1.jpg?1567231402',
+    },
+    {
+      item_id: '41',
+      image: 'https://static.mercdn.net/thumb/photos/m44620715627_1.jpg?1567489261',
+    },
+    {
+      item_id: '42',
+      image: 'https://static.mercdn.net/thumb/photos/m42864060416_1.jpg?1567747767',
+    },
+    {
+      item_id: '43',
+      image: 'https://static.mercdn.net/thumb/photos/m31285780594_1.jpg?1567598404',
+    },
+    {
+      item_id: '44',
+      image: 'https://static.mercdn.net/thumb/photos/m36940082217_1.jpg?1567322511',
+    },
+    {
+      item_id: '45',
+      image: 'https://static.mercdn.net/thumb/photos/m78949758466_1.jpg?1567253477',
+    },
+    {
+      item_id: '46',
+      image: 'https://static.mercdn.net/thumb/photos/m85787330636_1.jpg?1566138939',
+    },
+    {
+      item_id: '47',
+      image: 'https://static.mercdn.net/thumb/photos/m85787330636_1.jpg?1566138939',
+    },
+    {
+      item_id: '48',
+      image: 'https://static.mercdn.net/thumb/photos/m80301038244_1.jpg?1567747965',
+    },
+    {
+      item_id: '49',
+      image: 'https://static.mercdn.net/thumb/photos/m60905692820_1.jpg?1567747791',
+    },
+    {
+      item_id: '50',
+      image: 'https://static.mercdn.net/thumb/photos/m38984636354_1.jpg?1567747995',
+    },
+    {
+      item_id: '51',
+      image: 'https://static.mercdn.net/thumb/photos/m33648024473_1.jpg?1567607602',
+    },
+    {
+      item_id: '52',
+      image: 'https://static.mercdn.net/thumb/photos/m24730056847_1.jpg?1567742460',
+    },
+    {
+      item_id: '53',
+      image: 'https://static.mercdn.net/thumb/photos/m65277777370_1.jpg?1567742530',
+    },
+    {
+      item_id: '54',
+      image: 'https://static.mercdn.net/thumb/photos/m84186423493_1.jpg?1567742362',
+    },
+    {
+      item_id: '55',
+      image: 'https://static.mercdn.net/thumb/photos/m54337201526_1.jpg?1567742385',
+    },
+    {
+      item_id: '56',
+      image: 'https://static.mercdn.net/thumb/photos/m59222407664_1.jpg?1567519259',
+    },
+    {
+      item_id: '57',
+      image: 'https://static.mercdn.net/thumb/photos/m90493795585_1.jpg?1567692511',
+    },
+    {
+      item_id: '58',
+      image: 'https://static.mercdn.net/thumb/photos/m31008681209_1.jpg?1567742523',
+    },
+    {
+      item_id: '59',
+      image: 'https://static.mercdn.net/thumb/photos/m85437804275_1.jpg?1567712281',
+    },
+    {
+      item_id: '60',
+      image: 'https://static.mercdn.net/thumb/photos/m74754459766_1.jpg?1567519753',
+    },
+    {
+      item_id: '61',
+      image: 'https://static.mercdn.net/thumb/photos/m63037452152_1.jpg?1564491506',
+    },
+    {
+      item_id: '62',
+      image: 'https://static.mercdn.net/thumb/photos/m61966918151_1.jpg?1567226472',
+    },
+    {
+      item_id: '63',
+      image: 'https://static.mercdn.net/thumb/photos/m15507615267_1.jpg?1547620473',
+    },
+    {
+      item_id: '64',
+      image: 'https://static.mercdn.net/thumb/photos/m58770637989_1.jpg?1567167572',
+    },
+    {
+      item_id: '65',
+      image: 'https://static.mercdn.net/thumb/photos/m28899632827_1.jpg?1567479360',
+    },
+    {
+      item_id: '66',
+      image: 'https://static.mercdn.net/thumb/photos/m28671686495_1.jpg?1564994048',
+    },
+    {
+      item_id: '67',
+      image: 'https://static.mercdn.net/thumb/photos/m92513128788_1.jpg?1567493020',
+    },
+    {
+      item_id: '68',
+      image: 'https://static.mercdn.net/thumb/photos/m25553823710_1.jpg?1552724339',
+    },
+    {
+      item_id: '69',
+      image: 'https://static.mercdn.net/thumb/photos/m40768274914_1.jpg?1567040161',
+    },
+    {
+      item_id: '70',
+      image: 'https://static.mercdn.net/thumb/photos/m17636295167_1.jpg?1567730331',
+    }
+  ]
+)

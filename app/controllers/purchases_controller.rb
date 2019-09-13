576e4cbc9c5decb20edcd0b334b7f9253826b353
axela5001 <axela0104@au.com>
2019-09-13 17:13:46 +0900

    商品購入ページ＿クレジットカード

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,31 @@
 class PurchasesController < ApplicationController
+
+  require 'payjp'
+
   def edit
     @item = Item.find(params[:id])
     @user = User.find(current_user)
     @address = Address.find(current_user)
+    @card = CreditCard.find(current_user)
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    customer = Payjp::Customer.retrieve(@card.customer_id)
+    @default_card_information = customer.cards.retrieve(@card.card_id)
+    @exp_month = @default_card_information.exp_month.to_s
+    @exp_year = @default_card_information.exp_year.to_s.slice(2,3)
+  
   end
 
   def update
     @item = Item.find(params[:id])
+    
+    @card = CreditCard.find(current_user)
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp::Charge.create(
+    :amount => @item.price,
+    :customer => @card.customer_id,
+    :currency => 'jpy',
+  )
+
     @item.update(buyer_id: current_user.id)
     redirect_to controller: 'items', action: 'index'
   end

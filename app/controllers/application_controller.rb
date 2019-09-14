0ffdc3861a0a9f5df1a4eb207b8aad6bef8d07a5
河合宏紀 <hironori.kawai0720@gmail.com>
2019-09-14 09:36:56 +0900

    basic認証コメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth, if: :production?
+  # before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :category_id
 
@@ -11,11 +11,11 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  #   end
+  # end
 
   def category_id
     @parents = Category.where(ancestry: nil)
